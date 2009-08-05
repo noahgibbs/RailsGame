28e2a75d7c2794635d8eb3d574ae471df9b9aad4
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-08-04 19:27:57 -0700

    Make crypto keys file use URL-safe characters

--- generators/rg_crypto_keys/templates/crypto_keys.sh
+++ generators/rg_crypto_keys/templates/crypto_keys.sh
@@ -1,3 +1,3 @@
-export RM_REST_AUTH_SITE_KEY=<%= ActiveSupport::SecureRandom.base64(30) %>
-export RM_RAILS_SESSION_SECRET=<%= ActiveSupport::SecureRandom.base64(100) %>
-export RM_JUGGER_GS_SESSION=<%= ActiveSupport::SecureRandom.base64(40) %>
+export RM_REST_AUTH_SITE_KEY=<%= ActiveSupport::SecureRandom.hex(30) %>
+export RM_RAILS_SESSION_SECRET=<%= ActiveSupport::SecureRandom.hex(100) %>
+export RM_JUGGER_GS_SESSION=<%= ActiveSupport::SecureRandom.hex(40) %>
