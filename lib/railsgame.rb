5bb49d853480292d2c5fd0b9eede1fbd6084f670
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-08-07 21:48:29 -0700

    Rearrange stuff to use newgem for gems

--- lib/railsgame.rb
+++ lib/railsgame.rb
@@ -1,2 +1,6 @@
-# Railsgame
-#print "Included RailsGame library\n"
+#$:.unshift(File.dirname(__FILE__)) unless
+#  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+module RailsGame
+  VERSION = '0.0.1'
+end
