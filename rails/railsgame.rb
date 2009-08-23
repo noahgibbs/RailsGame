e21cb5e63c926204067e7d52e9dc7842e8b2b8e2
Noah Gibbs <noah_send_no_spam_gibbs@yahoo.com>
2009-08-23 00:16:56 -0700

    Update template

--- rails/railsgame.rb
+++ rails/railsgame.rb
@@ -1,23 +1,34 @@
 # This is a Rails template to set up a new RailsGame project
 
-def github_gem(user, gemname)
-  gem "#{user}-#{gemname}", :lib => gemname,
-    :source => "http://gems.github.com"
+def github_gem(user, gemname, options = {})
+  base_opts = { :lib => gemname, :source => "http://gems.github.com" }
+  base_opts.merge! options
+  gem "#{user}-#{gemname}", base_opts
 end
 
 plugin 'juggernaut_plugin', :git => 'git://github.com/maccman/juggernaut_plugin.git'
 plugin 'restful-authentication', :git => 'git://github.com/technoweenie/restful-authentication.git'
 
 gem "daemons"
-github_gem "noahgibbs", "railsgame"
-github_gem "maccman", "juggernaut_gem"
+github_gem "maccman", "juggernaut"
 
-rake("gems:install", :sudo => true)
+# Uncomment only one of these lines
+#github_gem "noahgibbs", "railsgame"
+gem "railsgame"
+#gem "noahgibbs-railsgame"  # assume already installed
+
+#rake("gems:install", :sudo => true)
 
 run "rm public/index.html"
 generate(:rg_crypto_keys)
+ENV['RM_RAILS_SESSION_SECRET'] = 'a' * 50  # Fake session secret
+
+generate(:rg_scaffold, 'game')
 route "map.root :controller => 'game'"
-rake("db:migrate")
+
+#generate(:rg_game_server)
+
+rake("db:migrate") # By default, this will be SQLite
 
 file '.gitignore', <<-END
 *~
@@ -29,6 +40,8 @@ db/*.sqlite3
 tmp/**/*
 pkg/*
 config/database.yml
+crypto_keys.sh
+private_variables.sh
 END
 
 #git :init
